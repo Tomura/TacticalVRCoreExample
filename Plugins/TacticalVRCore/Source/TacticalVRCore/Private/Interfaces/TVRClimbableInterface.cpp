e27b51e50235ae98a718cb458244b65fa2393c3b
tomura <tammo.beil@gmx.de>
2022-06-13 14:24:51 +0200

    remove tvrcore copy

--- Plugins/TacticalVRCore/Source/TacticalVRCore/Private/Interfaces/TVRClimbableInterface.cpp
+++
@@ -1,6 +0,0 @@
-// This file is covered by the LICENSE file in the root of this plugin.
-
-
-#include "Interfaces/TVRClimbableInterface.h"
-
-// Add default functionality here for any ITVRClimbableInterface functions that are not pure virtual.
--- Plugins/TacticalVRCore/Source/TacticalVRCore/Private/Weapon/Component/TVRMagazineHandSocketComponent.cpp
+++
@@ -1,9 +0,0 @@
-// This file is covered by the LICENSE file in the root of this plugin.
-
-#include "Weapon/Component/TVRMagazineHandSocketComponent.h"
-
-UTVRMagazineHandSocketComponent::UTVRMagazineHandSocketComponent(const FObjectInitializer& OI) : Super(OI)
-{
-	// MirrorAxis = EAxis::Y;
-	// FlipAxis = EAxis::Y;
-}
